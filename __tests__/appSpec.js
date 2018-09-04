ded3151bebcaf6cf8ff7351769e6f804ee3ec079
Gabriel Costa <gabriel.costa.goncalves@ibm.com>
2018-09-04 23:10:09 +0100

    Separate large chunks of code into smaller functions

--- __tests__/appSpec.js
+++ __tests__/appSpec.js
@@ -4,19 +4,71 @@ const app = require('../app');
 describe('validateUser', () => {
     test('Valid customer', () => {
         const customer = {
-            name: '',
+            name: 'John Doe',
             user_id: 1,
-            latitude: 1,
-            longitude: 1
+            latitude: '1',
+            longitude: '1'
         };
         const result = app.validateCustomer(customer);
         expect(result).toBeTruthy();
     });
 
-    test('Invalid customer', () => {
+    test('Empty customer', () => {
         const customer = {};
         expect(() => {
             app.validateCustomer(customer)
+        }).toThrow();
+    });
+
+    test('Invalid customer', () => {
+        const customer = {
+            name: 'John Doe',
+            user_id: 1,
+            latitude: '1N',
+            longitude: '1W'
+        };
+        expect(() => {
+            app.validateCustomer(customer)
+        }).toThrow();
+    });
+});
+
+describe('processLine', () => {
+    test('Valid line of near customer', () => {
+        const line = '{"latitude": "53.357250", "user_id": 1, "name": "John Doe", "longitude": "-6.251088"}',
+            customer = {
+                name: 'John Doe',
+                user_id: 1,
+                latitude: '53.357250',
+                longitude: '-6.251088'
+            },
+            result = app.processLine(line);
+        expect(result).toEqual(customer);
+    });
+
+    test('Valid line of far customer', () => {
+        const line = '{"latitude": "1.1", "user_id": 1, "name": "John Doe", "longitude": "-1.1"}',
+            customer = {
+                name: 'John Doe',
+                user_id: 1,
+                latitude: '1.1',
+                longitude: '-1.1'
+            },
+            result = app.processLine(line);
+        expect(result).toBeFalsy();
+    });
+
+    test('Invalid line', () => {
+        const line = '{"latitude": "1.1N", "user_id": 1, "name": "John Doe"}';
+        expect(() => {
+            app.validateCustomer(line);
+        }).toThrow();
+    });
+
+    test('Invalid JSON line', () => {
+        const line = 'This is not a proper client line.';
+        expect(() => {
+            app.validateCustomer(line);
         }).toThrow();
     });
 });
--- app.js
+++ app.js
@@ -9,53 +9,72 @@ function validateCustomer(customer) {
     if (!customer.hasOwnProperty('name')) {
         missingAttributes.push('name');
     }
-    if (!customer.hasOwnProperty('user_id')) {
+    if (!customer.hasOwnProperty('user_id') || isNaN(+customer.user_id)) {
         missingAttributes.push('user_id');
     }
-    if (!customer.hasOwnProperty('latitude')) {
+    if (!customer.hasOwnProperty('latitude') || isNaN(+customer.latitude)) {
         missingAttributes.push('latitude');
     }
-    if (!customer.hasOwnProperty('longitude')) {
+    if (!customer.hasOwnProperty('longitude') || isNaN(+customer.longitude)) {
         missingAttributes.push('longitude');
     }
     if (missingAttributes.length) {
-        throw 'Invalid customer. Missing attributes: ' + missingAttributes.join(', ');
+        throw 'Invalid customer. Missing/invalid attributes: ' + missingAttributes.join(', ');
     }
     return true;
 }
 
-function processFile(filename='customers.txt') {
+function processLine(line, lineNumber) {
     const dublinOffice = {
-            latitude: 53.339428,
-            longitude: -6.257664
-        },
-        closeCustomers = [];
+        latitude: 53.339428,
+        longitude: -6.257664
+    };
 
-    readline.createInterface({
-        input: fs.createReadStream(filename),
-        crlfDelay: Infinity
-    }).on('line', (line) => {
-        try {
-            const customer = JSON.parse(line);
-            validateCustomer(customer);
-            if (geo.areClose(customer, dublinOffice)) {
-                closeCustomers.push(customer);
-            }
-        } catch(error) {
-            console.error(error);
+    try {
+        const customer = JSON.parse(line);
+        validateCustomer(customer);
+        if (geo.areClose(customer, dublinOffice)) {
+            return customer;
         }
+        return null;
+    } catch(error) {
+        console.error(`Error processing input file line ${lineNumber}:`, error);
+        return null;
+    }
+}
+
+function processFile(stream) {
+    const closeCustomers = [];
+    let lineNumber = 1;
+
+    stream.on('line', (line) => {
+        const customer = processLine(line, lineNumber);
+        if (customer) {
+            closeCustomers.push(customer);
+        }
+        lineNumber = lineNumber + 1;
     }).on('close', () => {
-        let sortedCustomers = _.sortBy(closeCustomers, 'user_id');
+        let sortedCustomers = _.sortBy(closeCustomers, [(customer) => +customer.user_id]);
         _.forEach(sortedCustomers, (customer) => {
             console.log(`Name: ${customer.name}, User ID: ${customer.user_id}`);
         });
     });
 }
 
-if (typeof require != 'undefined' && require.main==module) {
-    processFile();
+function main(filename='customers.txt') {
+    const stream = readline.createInterface({
+        input: fs.createReadStream(filename),
+        crlfDelay: Infinity
+    });
+
+    processFile(stream);
+}
+
+if (typeof require != 'undefined' && require.main == module) {
+    main();
 }
 
 module.exports = {
-    validateCustomer
+    validateCustomer,
+    processLine
 }
